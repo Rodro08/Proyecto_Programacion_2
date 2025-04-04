ec8727f8ac343396f66b4b6b09b3cc58b044d984
Rodro08 <roodrigoaraque1002@gmail.com>
2025-04-03 23:10:09 -0600

    Actualizacion de informe

---
+++ src/main/java/DTO/ReservasDTO.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+import java.sql.Date;
+
+public class ReservasDTO {
+
+    private int idReserva;
+    private int idHuesped;
+    private int idHabitacion;
+    private Date fechaReserva;
+    private Date fechaIngreso;
+    private Date fechaSalida;
+
+    public ReservasDTO(int idReserva, int idHuesped, int idHabitacion, Date fechaReserva, Date fechaIngreso, Date fechaSalida) {
+        this.idReserva = idReserva;
+        this.idHuesped = idHuesped;
+        this.idHabitacion = idHabitacion;
+        this.fechaReserva = fechaReserva;
+        this.fechaIngreso = fechaIngreso;
+        this.fechaSalida = fechaSalida;
+    }
+
+    public int getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(int idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public int getIdHuesped() {
+        return idHuesped;
+    }
+
+    public void setIdHuesped(int idHuesped) {
+        this.idHuesped = idHuesped;
+    }
+
+    public int getIdHabitacion() {
+        return idHabitacion;
+    }
+
+    public void setIdHabitacion(int idHabitacion) {
+        this.idHabitacion = idHabitacion;
+    }
+
+    public Date getFechaReserva() {
+        return fechaReserva;
+    }
+
+    public void setFechaReserva(Date fechaReserva) {
+        this.fechaReserva = fechaReserva;
+    }
+
+    public Date getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public void setFechaIngreso(Date fechaIngreso) {
+        this.fechaIngreso = fechaIngreso;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+}
+
